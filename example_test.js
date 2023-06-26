45a80ea3efc2bea53d153fa46743b8c478ebd6a7
Gal Sesek <gal.sesek@endava.com>
2023-06-26 12:55:01 +0200

    Modified example_test.js file with assertion xmpls

--- example_test.js
+++ example_test.js
@@ -1,6 +1,9 @@
 Feature('example');
 
-Scenario('Should load example.com website',  ({ I }) => {
+Scenario('Should load zero.webappsecurity.com website',  ({ I }) => {
   I.amOnPage("http://zero.webappsecurity.com/");
   I.see("Zero Bank");
+  I.dontSee("Example");
+  I.seeElement("div");
+  I.dontSee("h1");
 });
