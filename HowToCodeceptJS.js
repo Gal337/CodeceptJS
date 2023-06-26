903630c5362aa37201d232bb87b63999e416fa19
Gal Sesek <gal.sesek@endava.com>
2023-06-26 13:11:02 +0200

    Added HowToCodeceptJS

---
+++ HowToCodeceptJS.js
@@ -0,0 +1,7 @@
+/*
+HOW TO INSTALL CODECEPTJS WITH PLAYWRIGHT:
+1. Create project
+2. Open Terminal
+3. Type: npm install playwright codeceptjs prettier in Terminal
+4. Wait for plugins to install
+*/
